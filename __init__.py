404aa896cd873a64f0b3fcdfdb6e4624f5dd833e
dadou000 <david.f.francois@gmail.com>
2026-08-31 20:03:00 +0200

    Add extension entry point

---
+++ __init__.py
@@ -0,0 +1,23 @@
+bl_info = {
+    "name": "Trees 2.0",
+    "author": "dadou000",
+    "version": (0, 1, 0),
+    "blender": (5, 2, 0),
+    "location": "3D View > Sidebar > Trees 2.0",
+    "description": "Procedural game-ready trees using low-poly branches and instanced 2D foliage cards",
+    "category": "Add Mesh",
+}
+
+from . import operators, properties, ui
+
+
+def register():
+    properties.register()
+    operators.register()
+    ui.register()
+
+
+def unregister():
+    ui.unregister()
+    operators.unregister()
+    properties.unregister()
